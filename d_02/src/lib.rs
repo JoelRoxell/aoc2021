989b1776c519bfa758f5c20dbd194ae84864422f
Joel S. Roxell <jroxell24@gmail.com>
2021-12-02 07:05:44 +0100

    feat(d_02): init

---
+++ d_02/src/lib.rs
@@ -0,0 +1,128 @@
+#[derive(Debug)]
+pub struct Sub {
+    horizontal: i32,
+    depth: i32,
+    aim: Option<i32>,
+}
+
+impl Sub {
+    pub fn forward(&mut self, n: i32) {
+        self.horizontal += n;
+
+        if let Some(aim) = self.aim {
+            self.depth += aim * n
+        }
+    }
+
+    pub fn down(&mut self, n: i32) {
+        if let Some(aim) = self.aim {
+            self.aim = Some(n + aim);
+        } else {
+            self.depth += n;
+        }
+    }
+
+    pub fn up(&mut self, n: i32) {
+        if let Some(aim) = self.aim {
+            self.aim = Some(aim - n);
+        } else {
+            self.depth -= n;
+        }
+    }
+
+    pub fn guide(&mut self, lines: Vec<String>) {
+        lines.iter().for_each(|cmd| {
+            let mut r = cmd.split(' ');
+            let action = r.next().expect("failed to read action");
+            let val = r.next().expect("failed to read val");
+            let val: i32 = val.parse().expect("failed to parse val");
+
+            match action {
+                "forward" => self.forward(val),
+                "down" => self.down(val),
+                "up" => self.up(val),
+                _ => panic!("failed to parse action: {}", action),
+            }
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Sub;
+    use shared::read_lines;
+
+    #[test]
+    fn d_02_a() {
+        let data = vec![
+            "forward 5".to_string(),
+            "down 5".to_string(),
+            "forward 8".to_string(),
+            "up 3".to_string(),
+            "down 8".to_string(),
+            "forward 2".to_string(),
+        ];
+
+        let mut s = Sub {
+            horizontal: 0,
+            depth: 0,
+            aim: None,
+        };
+
+        s.guide(data);
+
+        assert_eq!(150, s.horizontal * s.depth)
+    }
+
+    #[test]
+    fn d_02a() {
+        let data = read_lines("data/input.txt").unwrap();
+
+        let mut s = Sub {
+            horizontal: 0,
+            depth: 0,
+            aim: None,
+        };
+
+        s.guide(data);
+
+        assert_eq!(1693300, s.horizontal * s.depth)
+    }
+
+    #[test]
+    fn d_02_b() {
+        let data = vec![
+            "forward 5".to_string(),
+            "down 5".to_string(),
+            "forward 8".to_string(),
+            "up 3".to_string(),
+            "down 8".to_string(),
+            "forward 2".to_string(),
+        ];
+
+        let mut s = Sub {
+            horizontal: 0,
+            depth: 0,
+            aim: Some(0),
+        };
+
+        s.guide(data);
+
+        assert_eq!(900, s.horizontal * s.depth)
+    }
+
+    #[test]
+    fn d_02b() {
+        let data = read_lines("data/input.txt").unwrap();
+
+        let mut s = Sub {
+            horizontal: 0,
+            depth: 0,
+            aim: Some(0),
+        };
+
+        s.guide(data);
+
+        assert_eq!(1857958050, s.depth * s.horizontal);
+    }
+}
